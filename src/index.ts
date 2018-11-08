0c94de6d0e54759c3b893ab2c9ead83ddab68c0b
GyeongMin Lee <strotend@gmail.com>
2018-11-08 17:55:52 +0900

    index.ts renderContainerElement 함수로 변경, renderReactDOM 함수 제거

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,12 @@ import ReactDOM from "react-dom";
 
 import TodoApp from "./TodoApp";
 
-const containerId = "todo-app";
-renderDOM();
-renderReactDOM();
+const containerElement = renderContainerElement("todo-app");
+ReactDOM.render(React.createElement(TodoApp), containerElement);
 
-function renderDOM() {
-  const oldContainer = document.getElementById(containerId);
-  if (oldContainer) {
-    document.removeChild(oldContainer);
-  }
-  const container = document.createElement("div");
-  container.id = containerId;
-  document.body.appendChild(container);
-}
-
-function renderReactDOM() {
-  const container = document.getElementById(containerId);
-  ReactDOM.render(React.createElement(TodoApp), container);
+function renderContainerElement(containerId: string): HTMLElement {
+  const containerElement = window.document.createElement("div");
+  window.document.body.appendChild(containerElement);
+  containerElement.id = containerId;
+  return containerElement;
 }
