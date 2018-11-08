eb9f1628661228147ef0b10060e1bad3cf865695
GyeongMin Lee <strotend@gmail.com>
2018-11-08 12:30:55 +0900

    TodoApp.tsx 생성

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-import Person from "./Person";
+import React from "react";
+import ReactDOM from "react-dom";
 
-const person = new Person({ age: 24, name: "poppy" });
-console.log(person);
-person.sayHello();
+import TodoApp from "./TodoApp";
+
+const containerId = "todo-app";
+renderDOM();
+renderReactDOM();
+
+function renderDOM() {
+  const oldContainer = document.getElementById(containerId);
+  if (oldContainer) {
+    document.removeChild(oldContainer);
+  }
+  const container = document.createElement("div");
+  container.id = containerId;
+  document.body.appendChild(container);
+}
+
+function renderReactDOM() {
+  const container = document.getElementById(containerId);
+  ReactDOM.render(React.createElement(TodoApp), container);
+}
